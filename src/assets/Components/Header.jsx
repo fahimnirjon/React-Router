7abace96982d267e564d4808d7b62bf8b1c2dacd
fahimnirjon <fahimnirjon21@gmail.com>
2024-06-27 19:41:00 +0600

    few chnages made

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
              <nav>
                     <span>My Website</span>
                    <NavLink className="underline" to="/">Home</NavLink>
-                   <NavLink className="underline" to="/users">Users</NavLink>
+                   <NavLink to="/users">Users</NavLink>
                    <NavLink className="underline" to="/about">About</NavLink>
                    <NavLink  to="/contact">Contact</NavLink>
                 </nav>
